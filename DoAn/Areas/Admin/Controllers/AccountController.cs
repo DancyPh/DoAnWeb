8164b69b7e4f08e2d47daf185dd3a57e6272c29a
DancyPh <phongnguyenn121@gmail.com>
2023-11-15 12:57:15 +0700

    admin login

---
+++ DoAn/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAn.Models;
+
+namespace DoAn.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        QLPizzaDataContext data = new QLPizzaDataContext();
+        // GET: Admin/Account
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(NhanVien _userFormPage)
+		{
+            var acc = data.NhanViens.Where(m => m.taiKhoan == _userFormPage.taiKhoan && m.matKhau == _userFormPage.matKhau).FirstOrDefault();
+            if (acc == null)
+			{
+                ViewBag.LoginSatus = 0;
+			}
+			else
+			{
+                return RedirectToAction("Index", "Home");
+			}
+            return View();
+		}
+    }
+}
---
+++ DoAn/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
