859db4795ed0d57215d5dd547df5814cf9b1271c
Thọ Nguyễn <thonguyen20030615@gmail.com>
2023-12-08 12:41:16 +0700

    update code

--- DoAn/Controllers/DatBanController.cs
+++ DoAn/Controllers/DatBanController.cs
@@ -8,6 +8,31 @@ namespace DoAn.Controllers
 {
     public class DatBanController : Controller
     {
+        [HttpGet]
+        public ActionResult DatBan()
+        {
+            if (Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "")
+            {
+                return Redirect("~/User/DangNhap?id=2");
+            }
+            if (Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "")
+            {
+                return RedirectToAction("Dangnhap", "User");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DatHang(FormCollection f)
+        {
+           
+            return RedirectToAction("XacNhanDatBan", "DatBan");
+        }
+
+        public ActionResult XacNhanDatBan()
+        {
+            return View();
+        }
 
         // GET: DatBan
         public ActionResult Index()
